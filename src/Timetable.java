cac188b2ca7e444f0a59041b1c54e34db8e8b656
Dmitry Uglanov <dimarik.pay2009@yandex.ru>
2025-11-29 21:21:13 +0300

    for auto check code

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,20 +1,19 @@
 import java.util.*;
 
 public class Timetable {
-    private final HashMap<DayOfWeek, TreeMap<TimeOfDay, ArrayList<TrainingSession>>> timetable = new HashMap<>();;
+    private final HashMap<DayOfWeek, TreeMap<TimeOfDay, ArrayList<TrainingSession>>> timetable = new HashMap<>();
 
     public void addNewTrainingSession(TrainingSession trainingSession) {
-        if(timetable.containsKey(trainingSession.getDayOfWeek())) { // проверка на ключ "день недели"
-            if(timetable.get(trainingSession.getDayOfWeek()).
-                    containsKey(trainingSession.getTimeOfDay())) { // проверка на ключ "время дня"
-                timetable.get(trainingSession.getDayOfWeek()).
-                        get(trainingSession.getTimeOfDay()).
-                        add(trainingSession);
+        if (timetable.containsKey(trainingSession.getDayOfWeek())) { // проверка на ключ "день недели"
+            if (timetable.get(trainingSession.getDayOfWeek())
+                    .containsKey(trainingSession.getTimeOfDay())) { // проверка на ключ "время дня"
+                timetable.get(trainingSession.getDayOfWeek())
+                    .get(trainingSession.getTimeOfDay()). add(trainingSession);
             } else {
                 ArrayList<TrainingSession> trainingSessions = new ArrayList<>();
                 trainingSessions.add(trainingSession);
-                timetable.get(trainingSession.getDayOfWeek()).
-                        put(trainingSession.getTimeOfDay(), trainingSessions);
+                timetable.get(trainingSession.getDayOfWeek())
+                    .put(trainingSession.getTimeOfDay(), trainingSessions);
             }
         } else {
             TreeMap<TimeOfDay, ArrayList<TrainingSession>> dayTrainingSessions = new TreeMap<>();
@@ -47,7 +46,7 @@ public class Timetable {
         timetable.forEach((day, dayTrainings) -> {
             dayTrainings.forEach((time, timeTrainings) -> {
                 timeTrainings.forEach(training -> {
-                    if(countByCoach.containsKey(training.getCoach())) {
+                    if (countByCoach.containsKey(training.getCoach())) {
                         countByCoach.put(training.getCoach(), countByCoach.get(training.getCoach()) + 1);
                     } else {
                         countByCoach.put(training.getCoach(), 1);
@@ -57,7 +56,7 @@ public class Timetable {
         });
         // сортируем по количеству тренировок
         countByCoach.forEach((coach, count) -> {
-            if(counterOfTrainings.containsKey(count)) {
+            if (counterOfTrainings.containsKey(count)) {
                 counterOfTrainings.get(count).add(coach);
             } else {
                 counterOfTrainings.put(count, new ArrayList<>(List.of(coach)));
